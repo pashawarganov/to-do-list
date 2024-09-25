66f724ad27d29a115b7bfae6514e053f1a9174c2
Pasha <pavik.varganov@gmail.com>
2024-09-25 12:03:03 +0300

    rename task fields

--- to_do_list/models.py
+++ to_do_list/models.py
@@ -10,13 +10,14 @@ class Tag(models.Model):
 
 class Task(models.Model):
     content = models.CharField(max_length=200)
-    date_time = models.DateTimeField(auto_now_add=True)
-    deadline_date_time = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
     done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
 
     class Meta:
-        ordering = ["done", "-date_time"]
+        ordering = ["done", "-created"]
 
     def __str__(self):
-        return f"{self.content} - {self.date_time}({self.done})"
+        formatted_created = self.created.strftime("%Y-%m-%d %H:%M:%S")
+        return f"{self.content} - {formatted_created}({self.done})"
